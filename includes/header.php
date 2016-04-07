d852187c9e70513782003ce1bf0ab441e36c7784
Brian Crosby <croz1007@yahoo.com>
2016-04-07 19:13:45 -0400

    Added page endpoints with generic style

---
+++ includes/header.php
@@ -0,0 +1,11 @@
+<div id="hdr">
+  <h1 class="left">Header</h1>
+  <nav class="right">
+    <ul>
+      <li><a href="../residential">Residential</a></li>
+      <li><a href="../commercial">Commercial</a></li>
+      <li><a href="../about">About Us</a></li>
+      <li><a href="../contact">Contact Us</a></li>
+    </ul>
+  </nav>
+</div>
