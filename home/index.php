18dcf8c380edc3cfab8789e3b1660502840c9277
Brian Crosby <croz1007@yahoo.com>
2016-04-07 19:13:45 -0400

    Added page endpoints with generic style

---
+++ home/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<?php include_once("../includes/head.php") ?>
+<body>
+  <div id="wrapper">
+    <?php include_once("../includes/header.php") ?>
+    <div id="content" class="clear">
+      <h2>Home</h2>
+    </div>
+    <?php include_once("../includes/footer.php") ?>
+  </div>
+  <?php include_once("../includes/javascripts.php") ?>
+</body>
+</html>
---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+<div id="ftr">
+  <address>
+    2246 S. Hamilton Rd. Suite 101<br>
+    Columbus, OH 43232</br>
+    (614) 501-4444</br>
+  </address>
+</div>
