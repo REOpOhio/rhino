cc913a87429a86a5688bc225b91db68ed5b0c4ac
Brian Crosby <croz1007@yahoo.com>
2016-04-08 10:08:29 -0400

    Updated Footer styling to match Broadway theme

--- includes/head.php
+++ includes/head.php
@@ -1,3 +1,4 @@
 <head>
   <link rel="stylesheet" href="../assets/css/styles.css">
+  <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,700,400italic,700italic&subset=latin,greek' rel='stylesheet' type='text/css'>
 </head>
