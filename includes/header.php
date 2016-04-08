87ef147ffe1d579b315835e7746ec10afb071de1
Brian Crosby <croz1007@yahoo.com>
2016-04-08 12:40:26 -0400

    Header styling

--- includes/header.php
+++ includes/header.php
@@ -1,11 +1,15 @@
-<div id="hdr">
-  <h1 class="left title">Header</h1>
-  <nav class="right">
-    <ul>
-      <li><a href="../residential">Residential</a></li>
-      <li><a href="../commercial">Commercial</a></li>
-      <li><a href="../about">About Us</a></li>
-      <li><a href="../contact">Contact Us</a></li>
-    </ul>
-  </nav>
+<div class="super-container header">
+  <div class="container">
+    <div class="logo">
+      <h1 class="left title">Header</h1>
+    </div>
+    <nav class="right">
+      <ul>
+        <li><a href="../residential">Residential</a></li>
+        <li><a href="../commercial">Commercial</a></li>
+        <li><a href="../about">About Us</a></li>
+        <li><a href="../contact">Contact Us</a></li>
+      </ul>
+    </nav>
+  </div>
 </div>
