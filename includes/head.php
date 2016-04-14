0b818fb9a2502a53962f0327e152ff41eec369b2
Brian Crosby <croz1007@yahoo.com>
2016-04-14 10:48:51 -0400

    Added meta

--- includes/head.php
+++ includes/head.php
@@ -1,2 +1,4 @@
 <link rel="stylesheet" href="../assets/css/styles.css">
 <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,700,400italic,700italic&subset=latin,greek' rel='stylesheet' type='text/css'>
+<meta name="keywords" content="real estate, real estate opportunity, Columbus, Ohio, property management, rental, property, Columbus Ohio real estate">
+<meta name="owner" content="Jeffery Keller">
