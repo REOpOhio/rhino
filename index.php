816f73b4f08e11216958f50b2ea1213d6bfba121
Brian Crosby <croz1007@yahoo.com>
2016-04-14 10:37:09 -0400

    Moved index out of directory

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" href="assets/css/styles.css">
+  <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,700,400italic,700italic&subset=latin,greek' rel='stylesheet' type='text/css'>
+</head>
+
+<body>
+  <div id="wrapper">
+    <div class="super-container header">
+      <div class="container">
+        <div class="logo">
+          <h1 class="left title"><a href="./">Real Estate Opportunity</a></h1>
+        </div>
+        <nav class="right">
+          <ul>
+            <li><a href="./residential">Residential</a></li>
+            <li><a href="./commercial">Commercial</a></li>
+            <li><a href="./about">About Us</a></li>
+            <li><a href="./contact">Contact Us</a></li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+    <div id="content" class="clear">
+      <h2>Home</h2>
+    </div>
+    <?php include_once("includes/footer.php") ?>
+  </div>
+  <?php include_once("includes/javascripts.php") ?>
+</body>
+<?php include_once("analyticstracking.php") ?>
+</html>
