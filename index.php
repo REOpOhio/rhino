94e23e16f91e3fa7c49701700c523a036b5a0539
Brian Crosby <croz1007@yahoo.com>
2016-04-14 10:50:37 -0400

    Updated keywords

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
   <title>Real Estate Opportunity - Home</title>
   <link rel="stylesheet" href="assets/css/styles.css">
   <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,700,400italic,700italic&subset=latin,greek' rel='stylesheet' type='text/css'>
-  <meta name="keywords" content="real estate, real estate opportunity, Columbus, Ohio, property management, rental, property, Columbus Ohio real estate">
+  <meta name="keywords" content="real estate, real estate opportunity, Columbus, Ohio, property management, rental, property, Columbus Ohio real estate, residential, commercial, commercial real estate, residential real estate">
   <meta name="owner" content="Jeffery Keller">
 </head>
 
