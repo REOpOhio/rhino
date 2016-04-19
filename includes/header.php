6f4d0ad005a51611ade848661f5bac833c299bab
Brian Crosby <croz1007@yahoo.com>
2016-04-19 19:29:03 -0400

    Added logo

--- includes/header.php
+++ includes/header.php
@@ -1,12 +1,14 @@
 <div class="super-container header">
   <div class="container">
     <div class="logo">
-      <h1 class="left title"><a href="../">Real Estate Opportunity</a></h1>
+      <a href="../"><img src="../assets/images/REO_Logo_bordered_small.png"></a>
     </div>
     <nav class="right">
       <ul>
+        <li><a href="../">Home</a></li>
         <li><a href="../residential">Residential</a></li>
         <li><a href="../commercial">Commercial</a></li>
+        <li><a href="./rental">Property Management</a></li>
         <li><a href="../about">About Us</a></li>
         <li><a href="../contact">Contact Us</a></li>
       </ul>
